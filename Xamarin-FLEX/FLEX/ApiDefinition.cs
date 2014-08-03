4e1516e6738835a8f19c9fe50f9cefe7dd871fe4
Rocky Duan <dementrock@gmail.com>
2014-08-02 17:57:38 -0700

    add xamarin binding

---
+++ Xamarin-FLEX/FLEX/ApiDefinition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace FLEX {
+
+  [BaseType (typeof (NSObject))]
+  public partial interface FLEXManager {
+
+    [Static, Export ("sharedManager")]
+    FLEXManager SharedManager { get; }
+
+    [Export ("isHidden")]
+    bool IsHidden { get; }
+
+    [Export ("showExplorer")]
+    void ShowExplorer ();
+
+    [Export ("hideExplorer")]
+    void HideExplorer ();
+
+  }
+}
